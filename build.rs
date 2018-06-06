0d3d6a8779210fc1d937d7b02a07bd500d39ee29
Jonas Olson <jolson@kth.se>
2018-06-06 21:20:14 +0200

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,41 +1,43 @@
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::fs::File;
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let f_in = BufReader::new(File::open("src/allascii.txt").unwrap());
     let mut f_out = File::create(env::var("OUT_DIR").unwrap() + "/table.rs").unwrap();
 
-    for line in
-        f_in.lines()
+    for line in f_in.lines()
         .map(|x| x.unwrap())
         .skip_while(|x| !x.contains("-----"))
-        .skip(1) {
-            let mut words = line.split("  ")
-                .map(|x| x.trim())
-                .filter(|x| !x.is_empty());
-            let original_name = words.next().unwrap();
-            let name = original_name
-                .replace("{220} lattice spacing of silicon",
-                         "LATTICE_SPACING_220_OF_SILICON")
-                .replace("mom.um", "momentum")
-                .replace(" ", "_")
-                .replace("-", "_")
-                .replace(".", "")
-                .replace(",", "")
-                .replace("(", "")
-                .replace(")", "")
-                .replace("/", "_PER_")
-                .to_uppercase();
-            let val = words.next().unwrap()
-                .replace(" ", "")
-                .replace("...", "");
-            let unit = match words.skip(1).next() {
-                Some(u) => format!("unit: {}", u),
-                None => "dimensionless".to_owned()
-            };
-            f_out.write_fmt(format_args!("/// {} ({})\npub const {}: f64 = {}f64;\n",
-                                         original_name, unit, name, val)).unwrap();
-        }
+        .skip(1)
+    {
+        let mut words = line.split("  ").map(|x| x.trim()).filter(|x| !x.is_empty());
+        let original_name = words.next().unwrap();
+        let name = original_name
+            .replace(
+                "{220} lattice spacing of silicon",
+                "LATTICE_SPACING_220_OF_SILICON",
+            )
+            .replace("mom.um", "momentum")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace(".", "")
+            .replace(",", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("/", "_PER_")
+            .to_uppercase();
+        let val = words.next().unwrap().replace(" ", "").replace("...", "");
+        let unit = match words.skip(1).next() {
+            Some(u) => format!("unit: {}", u),
+            None => "dimensionless".to_owned(),
+        };
+        f_out
+            .write_fmt(format_args!(
+                "/// {} ({})\npub const {}: f64 = {}f64;\n",
+                original_name, unit, name, val
+            ))
+            .unwrap();
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -26,59 +26,50 @@ mod tests {
     #[test]
     fn test_exact_quantity() {
         // An exact quantity whose decimal expansion ends with "..." in the table
-        assert_eq!(SPEED_OF_LIGHT_IN_VACUUM,
-                   299792458f64);
+        assert_eq!(SPEED_OF_LIGHT_IN_VACUUM, 299792458f64);
     }
 
     #[test]
     fn test_no_exponent() {
         // A value that has no exponent in the listing
-        assert_eq!(ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV,
-                   931.4940954);
+        assert_eq!(ATOMIC_MASS_CONSTANT_ENERGY_EQUIVALENT_IN_MEV, 931.4940954);
     }
 
     #[test]
     fn test_positive_exponent() {
-        assert_eq!(BOLTZMANN_CONSTANT_IN_HZ_PER_K,
-                   2.0836612e10);
+        assert_eq!(BOLTZMANN_CONSTANT_IN_HZ_PER_K, 2.0836612e10);
     }
 
     #[test]
     fn test_negative_exponent() {
-        assert_eq!(CLASSICAL_ELECTRON_RADIUS,
-                   2.8179403227e-15);
+        assert_eq!(CLASSICAL_ELECTRON_RADIUS, 2.8179403227e-15);
     }
 
     #[test]
     fn test_negative_value() {
-        assert_eq!(ELECTRON_CHARGE_TO_MASS_QUOTIENT,
-                   -1.758820024e11);
+        assert_eq!(ELECTRON_CHARGE_TO_MASS_QUOTIENT, -1.758820024e11);
     }
 
     #[test]
     fn test_dimensionless_value() {
-        assert_eq!(PROTON_ELECTRON_MASS_RATIO,
-                   1836.15267389);
+        assert_eq!(PROTON_ELECTRON_MASS_RATIO, 1836.15267389);
     }
 
     #[test]
     fn test_first_quantity() {
         // The first quantity listed in the table
-        assert_eq!(LATTICE_SPACING_220_OF_SILICON,
-                   192.0155714e-12);
+        assert_eq!(LATTICE_SPACING_220_OF_SILICON, 192.0155714e-12);
     }
 
     #[test]
     fn test_last_quantity() {
         // The last quantity listed in the table
-        assert_eq!(WIEN_WAVELENGTH_DISPLACEMENT_LAW_CONSTANT,
-                   2.8977729e-3);
+        assert_eq!(WIEN_WAVELENGTH_DISPLACEMENT_LAW_CONSTANT, 2.8977729e-3);
     }
 
     #[test]
     fn test_changed_name() {
         // This quantity is called "{220} lattice spacing of silicon" in the original listing. To get a valid identifier, which must not contain curly brackets and must not begin with a digit, its name has to be changed entirely, not just by a simple character replacement. This test checks that the new name came through as it should.
-        assert_eq!(LATTICE_SPACING_220_OF_SILICON,
-                   192.0155714e-12);
+        assert_eq!(LATTICE_SPACING_220_OF_SILICON, 192.0155714e-12);
     }
 }
